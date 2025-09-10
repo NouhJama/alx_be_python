34c0ed2130bc2e36a6aa8faae250a1ce128ece04
Nouh <noohahmed600@gmail.com>
2025-09-10 13:35:13 +0300

    Initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1= 5
+number2= 10
+
+addition = number1 + number2
+subtraction = number2 - number1
+multiplication = number1 * number2
+
+print(f"Addition of {number1} and {number2} is: {addition}")
+print(f"Subtraction of {number1} and {number2} is: {subtraction}")
+print(f"Multiplication of {number1} and {number2} is: {multiplication}")
+

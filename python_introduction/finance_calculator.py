4bdd429cc62430c751c3646bb74ee10cc681072a
Nouh <noohahmed600@gmail.com>
2025-09-10 16:57:01 +0300

    corrected prompts and calculations in finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,8 @@
-monthly_income = int(input("What is your monthly income? "))
-total_monthly_expenses = int(input("What are your total monthly expenses? "))
+monthly_income = int(input("Enter your monthly income: "))
+total_monthly_expenses = int(input("Enter your total monthly expenses: "))
 monthly_savings = monthly_income - total_monthly_expenses
 
-interest_rate = 0.05
-annual_savings = monthly_savings * 12 + (monthly_savings * 12 * interest_rate)
+annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 print(f"Your monthly savings are: {monthly_savings}")
 print(f"Projected savings after one year, with interest, is: {annual_savings:.0f}.")

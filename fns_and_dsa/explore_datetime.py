e2658b6c6cddc3ad1b0992b0c79d563fae4fc545
Nouh <noohahmed600@gmail.com>
2025-09-23 13:31:35 +0300

    Added explore_datetime.py!

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,17 @@
+import datetime
+
+def display_current_datetime():
+    # Display the current date and time in a readable format
+    current_date = datetime.datetime.now()
+    print("Current date and time:", current_date.strftime("%Y-%m-%d %H:%M:%S"))
+
+display_current_datetime()
+
+def calculate_future_date(days_ahead):
+    # Calculate the date a certain number of days from today
+    future_date = datetime.datetime.now() + datetime.timedelta(days=days_ahead)
+    return future_date.strftime("%Y-%m-%d") 
+
+days = int(input("Enter number of days to add to the current date: "))
+future_date = calculate_future_date(days)
+print("Future date:", future_date)

b70133c0228fd776ddfc9f9145fa9d808ce0f43f
Nouh <noohahmed600@gmail.com>
2025-10-01 20:55:17 +0300

    added a programming_paradigm folder and implemented bank_account.py and main-0.py files

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount:
+    def __init__(self, acount_balance=0):
+        self.account_balance = acount_balance
+
+    def deposit(self, amount):
+        return amount + self.account_balance
+    
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
+            return self.account_balance - amount
+        else:
+            return False
+    
+    def display_balance(self):
+        return f"Current Balance: ${self.account_balance}"
+    
+
+
+        

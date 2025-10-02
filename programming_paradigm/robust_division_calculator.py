45adad26489a23da2e369236de6d44b099828de7
Nouh <noohahmed600@gmail.com>
2025-10-02 11:01:04 +0300

    Added main.py and robust_division_calculator.py and implemented division function

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,23 @@
+def safe_divide(numerator, denominator):
+    try:
+        result = float(numerator) / float(denominator)
+    except ZeroDivisionError:
+        print("Error: can't divide by zero")
+    except ValueError:
+        print('Invalid input')
+    else:
+        return f"The result of the division is {result:.1f}"
+
+def main():
+    numb1 = input('Enter numerator: ')
+    numb2 = input('Enter denominator: ')
+    result = safe_divide(numb1, numb2)
+    if result is not None:
+        print(f"The result of the division is {result:.1f}")
+
+if __name__ == "__main__":
+    main()
+
+
+    
+              
